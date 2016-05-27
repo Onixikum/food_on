57ee8e46c8911dde8e8cd6e3df3db3ca71307a7d
OniX <dragonix1988@gmail.com>
2016-05-27 10:29:41 +0300

    Static

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,32 +2,20 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-	let(:base_title) { "FoodON" }
+  subject { page }
 
   describe "Home page" do
+    before { visit '/' }
 
-    it "should have the content 'Menu of the day' " do
-      visit '/static_pages/home'
-      expect(page).to have_content('Menu of the day')
-    end
-
-    it "should have the title 'Home' " do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("#{base_title}")
-    end
+    it { should have_content('FoodON') }
+    it { should have_title(full_title('')) }
   end
 
   describe "Help page" do
+    before { visit '/help' }
 
-  	it "should have the content 'Help info' " do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help info')
-  	end
-
-  	it "should have the title 'Help' " do
-  		visit '/static_pages/help'
-  		expect(page).to have_title("#{base_title} | Help")
-  	end
+    it { should have_content('Help Info') }
+    it { should have_title(full_title('Help')) }
   end
   
 end
