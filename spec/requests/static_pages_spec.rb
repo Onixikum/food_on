cea600754cc46485792ff8d5105a7524ff687895
OniX <dragonix1988@gmail.com>
2016-05-27 13:58:23 +0300

    modern test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,18 +4,43 @@ describe "Static Pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'FoodON' }
+    let(:page_title) { '' }
 
-    it { should have_content('FoodON') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)    { 'Help Info' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help Info') }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
+  end
+
+  it "shoud have the right links on the layout" do
+    visit root_path
+    first('.container').click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    first('.container').click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign Up'))
+    first('.container').click_link "FoodON"
+    expect(page).to have_title(full_title(''))
+    first('.footer').click_link "FoodON"
+    expect(page).to have_title(full_title(''))
+    first('.footer').click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    first('.footer').click_link "Home"
+    expect(page).to have_title(full_title(''))
   end
   
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Usser pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it { should have_selector('h1', text: 'Sign Up') }
+    it { should have_title(full_title('Sign Up')) }
   end
 end
