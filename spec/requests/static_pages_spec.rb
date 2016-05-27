31f09304b2b7841b6e1496d6f3e661b2041b2df7
OniX <dragonix1988@gmail.com>
2016-05-27 11:47:20 +0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,14 +5,14 @@ describe "Static Pages" do
   subject { page }
 
   describe "Home page" do
-    before { visit '/' }
+    before { visit root_path }
 
     it { should have_content('FoodON') }
     it { should have_title(full_title('')) }
   end
 
   describe "Help page" do
-    before { visit '/help' }
+    before { visit help_path }
 
     it { should have_content('Help Info') }
     it { should have_title(full_title('Help')) }
